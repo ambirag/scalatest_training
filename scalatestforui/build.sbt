4d8b35cdae6e090edf9916ddfd215d0f7cd62d26
Ragavan Ambighananthan <rambighananthan@expedia.com>
2016-09-27 16:01:27 +0100

    html report support, tags

--- scalatestforui/build.sbt
+++ scalatestforui/build.sbt
@@ -3,10 +3,9 @@ name := "scalatestforui"
 version := "1.0"
 
 scalaVersion := "2.11.8"
-//scalaVersion := "2.10.3"
-//libraryDependencies +=  "org.seleniumhq.selenium" % "selenium-java" % "2.53.0" % "test"
-//libraryDependencies += "org.scalatest" % "scalatest_2.11" % "2.2.1" %  "org.seleniumhq.selenium" % "test"
-//libraryDependencies += "org.scalatest" %% "scalatest"  % "test"
-//libraryDependencies += "org.seleniumhq.selenium" % "org.scalatest" % "scalatest" % "3.0.0" % "test"
+
 libraryDependencies += "org.seleniumhq.selenium" % "selenium-java" % "2.53.0" % "test"
-libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.0"
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.0" % "test->*" excludeAll (
+ExclusionRule(organization="org.junit", name="junit")
+)
+testOptions in Test += Tests.Argument(TestFrameworks.ScalaTest, "-h", "target/test-reports")
--- scalatestforui/src/test/scala/helloworld/spec/HelloWorld.scala
+++ scalatestforui/src/test/scala/helloworld/spec/HelloWorld.scala
@@ -2,48 +2,60 @@
 package helloworld.spec
 
 import org.openqa.selenium.interactions.Actions
-import org.openqa.selenium.{By, WebDriver,WebElement}
+import org.openqa.selenium.{By, WebElement, WebDriver}
 import org.openqa.selenium.firefox.FirefoxDriver
-import org.scalatest.{FeatureSpec, _}
+import org.scalatest.concurrent.Eventually
 import org.scalatest.selenium.WebBrowser
+import org.scalatest.{FeatureSpec, _}
 
 import scala.util.Try
 
 /**
   * Created by rambighananthan on 9/26/16.
   */
-class HelloWorld extends FeatureSpec with GivenWhenThen with Matchers with WebBrowser {
 
-   implicit val webDriver: WebDriver = new FirefoxDriver()
+abstract class AcceptanceSpec extends FeatureSpec with GivenWhenThen with Matchers with WebBrowser with Eventually {
 
-    val host = "http://www.expedia.co.uk/"
-     info("Making sure that the docweb site is working ")
+}
 
-    feature("Hotel Search") {
-      scenario(s"Search for a Hotel from Homepageof UK") {
-      Given("The user lands on the homepage")
-        go to host
-        assertResult(true,"Home page loading takes more time ") {
+object Smoke extends Tag("com.expedia.Smoke")
+
+class HelloWorld extends AcceptanceSpec {
+
+  implicit val webDriver: WebDriver = new FirefoxDriver()
+
+  val host = "http://www.expedia.ie/"
+  info("Making sure that the docweb site is working ")
+
+ feature("Hotel Search") {
+    scenario(s"Search for a Hotel from Homepageof UK",Smoke) {
+     Given("The user lands on the homepage")
+
+      go to host
+      assertResult(true,"Home page loading takes more time ") {
+        eventually {
           Try(executeScript("return document.readyState == 'complete'")(webDriver).toString().toBoolean).getOrElse(false)
         }
+      }
 
-        When("The user enters a destination")
-         val hotelTab: WebElement =Try(webDriver.findElement(By.id("tab-hotel-tab"))).getOrElse(null)
-         click on hotelTab
-         var destination: TextField = Try(textField("hotel-destination")(webDriver)).getOrElse(null)
-         new Actions(webDriver).moveToElement(webDriver.findElement(By.id("hotel-destination"))).perform();
-         destination.value ="Las Vegas"
-         pageTitle should include("Travel Deals")
+      When("The user enters a destination")
+      val hotelTab: WebElement =Try(webDriver.findElement(By.id("tab-hotel-tab"))).getOrElse(null)
+      click on hotelTab
+      var destination: TextField = Try(textField("hotel-destination")(webDriver)).getOrElse(null)
+      new Actions(webDriver).moveToElement(webDriver.findElement(By.id("hotel-destination"))).perform();
+      //textField("hotel-destination").value = "Cheese!"
+      destination.value ="Las Vegas"
+      pageTitle should include("Hotels: from cheap")
 
-         And ("Click on search")
-         val searchButton: WebElement = Try(webDriver.findElement(By.id("search-button"))).getOrElse(null)
-         click on searchButton
+     And ("Click on search")
+      val searchButton: WebElement = Try(webDriver.findElement(By.id("search-button"))).getOrElse(null)
+      click on searchButton
 
-         Then ("User should land on Hotel Search Results page")
-         currentUrl.contains("Hotel-Search")
-
-    }
-    }
+     Then ("User should land on Hotel Search Results page")
+      currentUrl.contains("Hotel-Search")
+      quit()
+     }
+  }
 
 }
 
